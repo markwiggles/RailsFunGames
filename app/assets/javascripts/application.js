d6531641db566454c2ccb680cdab9acdb1ac7c0c
markwiggles <markwigg@live.com>
2014-08-20 16:51:30 +1000

    added background test

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,26 +16,3 @@
 //= require_tree .
 
 
-$(window).load(function() {
-
-    var theWindow        = $(window),
-        $bg              = $("#image-bg"),
-        aspectRatio      = $bg.width() / $bg.height();
-
-    function resizeBg() {
-
-        if ( (theWindow.width() / theWindow.height()) < aspectRatio ) {
-            $bg
-                .removeClass()
-                .addClass('bgheight');
-        } else {
-            $bg
-                .removeClass()
-                .addClass('bgwidth');
-        }
-
-    }
-
-    theWindow.resize(resizeBg).trigger("resize");
-
-});
