d1fa1e89158532afe4734838f41dbb43f82fd586
markwiggles <markwigg@live.com>
2014-08-20 19:37:54 +1000

    added video

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,46 @@
 //= require_tree .
 
 
+
+
+$('#play-button')
+    .hover(function () {
+        $(this).stop().animate({ width: 105, height: 105, opacity: 1.0 }, 'fast');
+    }, function () {
+        $(this).stop().animate({ width: 100, height: 100, opacity: 0.7 }, 'fast');
+    });
+
+
+$('#play-button')
+    .click(function () {
+        $('.video-container').fadeIn('slow');
+        $('#play-button').hide();
+        $('#page-content').css({opacity: 0.25})
+    });
+$('#intro-close')
+    .click(function () {
+        $('.video-container').hide();
+        $('#play-button').show();
+        $('#page-content').css({opacity: 1.0})
+
+    })
+
+
+
+// Load the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// Replace the 'ytplayer' element with an <iframe> and
+// YouTube player after the API code downloads.
+var player;
+function onYouTubePlayerAPIReady() {
+    player = new YT.Player('ytplayer', {
+        height: '390',
+        width: '640',
+        videoId: 'r0LnnSU68Cs'
+    });
+
+}
